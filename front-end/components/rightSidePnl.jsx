4c495214135520c80c0958353a0763d5bdd5ddf8
Shahidul Islam <shahidul1920shakil@gmail.com>
2026-07-11 17:42:25 +0600

    Show article metadata on post pages
    Fetch additional `articleMetadata` fields in the post query and render them in a bordered details panel. Also adds a right-side panel stub and reserves space on the home page for a main news preview layout.

---
+++ front-end/components/rightSidePnl.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const rightSidePnl = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default rightSidePnl
--- front-end/pages/Home.jsx
+++ front-end/pages/Home.jsx
@@ -4,7 +4,16 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
+
+
       <HeroNews />
+
+      {/* Main News Preview Panel */}
+      <section className='flex flex-col md:flex-row gap-4 mt-4'>
+
+      </section>
+
+
     </div>
   )
 }
