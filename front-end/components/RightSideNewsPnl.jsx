119d3d102bc9aa3e2b8a9fb3c1127c85560b429d
Shahidul Islam <shahidul1920shakil@gmail.com>
2026-07-12 19:08:12 +0600

    temp ready

--- front-end/components/RightSideNewsPnl.jsx
+++ front-end/components/RightSideNewsPnl.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const RightSideNewsPnl = () => {
     return (
-        <div className='rightSideNewsPnl font-bangali'>
+        <div className='rightSideNewsPnl font-bangali grid gap-6'>
 
             <section className="top1sec grid grid-cols-2 gap-4 font-bangali border-b-1 border-brandborder pb-4 items-center max-h-[400px]">
                 <div className="leftt h-full">
@@ -28,16 +28,192 @@ const RightSideNewsPnl = () => {
                 </div>
             </section>
 
-            <section className="top2sec grid grid-cols-2 gap-4 font-bangali pt-4">
+            <section className="top2sec grid grid-cols-2 gap-4 font-bangali pb-4 border-b-1 border-brandborder">
 
-                <div className="leftt">
+                <div className="lefttNews grid grid-cols-2 gap-4 border-r-1 border-brandborder pr-4">
+                    <div className="image">
+                        <Image
+                            src="http://localhost/astha-news/wp-content/uploads/2026/07/prothomalo-bangla_2026-07-11_mpg7sr81_US-Iran-2.jpg"
+                            alt="abc"
+                            width={350}
+                            height={250}
+                            className="object-cover size-full transition-transform duration-300 group-hover:scale-105"
+                        />
+                    </div>
+                    <div className="text">
+                        <h3 className="text-[20px] font-bold text-gray-900">
+                            মার্কিন বোমা হামলা ও প্রাথমিক সমঝোতা ব্যর্থ, ইরান নিয়ে ট্রাম্পের পরের পরিকল্পনা কী
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa dolores modi eveniet totam, quis possimus minima distinctio, tempore numquam, ducimus vitae hic omnis rerum perspiciatis porro dicta maiores illo ab!
+                        </p>
+                        <p className="mt-2 text-[16px] text-gray-500">
+                            ১১ জুলাই, ২০২৬ এ ১১:২৬ AM
+                        </p>
+                    </div>
 
                 </div>
 
-                <div className="rightt">
-                    
+                <div className="righttNews grid grid-cols-2 gap-4">
+                    <div className="image">
+                        <Image
+                            src="http://localhost/astha-news/wp-content/uploads/2026/07/prothomalo-bangla_2026-07-11_mpg7sr81_US-Iran-2.jpg"
+                            alt="abc"
+                            width={350}
+                            height={250}
+                            className="object-cover size-full transition-transform duration-300 group-hover:scale-105"
+                        />
+                    </div>
+                    <div className="text">
+                        <h3 className="text-[20px] font-bold text-gray-900">
+                            মার্কিন বোমা হামলা ও প্রাথমিক সমঝোতা ব্যর্থ, ইরান নিয়ে ট্রাম্পের পরের পরিকল্পনা কী
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa dolores modi eveniet totam, quis possimus minima distinctio, tempore numquam, ducimus vitae hic omnis rerum perspiciatis porro dicta maiores illo ab!
+                        </p>
+                        <p className="mt-2 text-[16px] text-gray-500">
+                            ১১ জুলাই, ২০২৬ এ ১১:২৬ AM
+                        </p>
+                    </div>
                 </div>
 
+            </section>
+
+            <section className="grid6sec grid grid-cols-3 grid-rows-2 gap-4 font-bangali">
+                <div className="one flex flex-col gap-2">
+                    <div className="image">
+                        <Image
+                            src="http://localhost/astha-news/wp-content/uploads/2026/07/prothomalo-bangla_2026-07-11_mpg7sr81_US-Iran-2.jpg"
+                            alt="abc"
+                            width={350}
+                            height={250}
+                            className="object-cover size-full transition-transform duration-300 group-hover:scale-105"
+                        />
+                    </div>
+                    <div className="text">
+                        <h3 className="text-[20px] font-bold text-gray-900">
+                            মার্কিন বোমা হামলা ও প্রাথমিক সমঝোতা ব্যর্থ, ইরান নিয়ে ট্রাম্পের পরের পরিকল্পনা কী
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa dolores modi eveniet totam, quis possimus minima distinctio, tempore numquam, ducimus vitae hic omnis rerum perspiciatis porro dicta maiores illo ab!
+                        </p>
+                        <p className="mt-2 text-[16px] text-gray-500">
+                            ১১ জুলাই, ২০২৬ এ ১১:২৬ AM
+                        </p>
+                    </div>
+                </div>
+                <div className="two flex flex-col gap-2">
+                    <div className="image">
+                        <Image
+                            src="http://localhost/astha-news/wp-content/uploads/2026/07/prothomalo-bangla_2026-07-11_mpg7sr81_US-Iran-2.jpg"
+                            alt="abc"
+                            width={350}
+                            height={250}
+                            className="object-cover size-full transition-transform duration-300 group-hover:scale-105"
+                        />
+                    </div>
+                    <div className="text">
+                        <h3 className="text-[20px] font-bold text-gray-900">
+                            মার্কিন বোমা হামলা ও প্রাথমিক সমঝোতা ব্যর্থ, ইরান নিয়ে ট্রাম্পের পরের পরিকল্পনা কী
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa dolores modi eveniet totam, quis possimus minima distinctio, tempore numquam, ducimus vitae hic omnis rerum perspiciatis porro dicta maiores illo ab!
+                        </p>
+                        <p className="mt-2 text-[16px] text-gray-500">
+                            ১১ জুলাই, ২০২৬ এ ১১:২৬ AM
+                        </p>
+                    </div>
+                </div>
+                <div className="three flex flex-col gap-2">
+                    <div className="image">
+                        <Image
+                            src="http://localhost/astha-news/wp-content/uploads/2026/07/prothomalo-bangla_2026-07-11_mpg7sr81_US-Iran-2.jpg"
+                            alt="abc"
+                            width={350}
+                            height={250}
+                            className="object-cover size-full transition-transform duration-300 group-hover:scale-105"
+                        />
+                    </div>
+                    <div className="text">
+                        <h3 className="text-[20px] font-bold text-gray-900">
+                            মার্কিন বোমা হামলা ও প্রাথমিক সমঝোতা ব্যর্থ, ইরান নিয়ে ট্রাম্পের পরের পরিকল্পনা কী
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa dolores modi eveniet totam, quis possimus minima distinctio, tempore numquam, ducimus vitae hic omnis rerum perspiciatis porro dicta maiores illo ab!
+                        </p>
+                        <p className="mt-2 text-[16px] text-gray-500">
+                            ১১ জুলাই, ২০২৬ এ ১১:২৬ AM
+                        </p>
+                    </div>
+                </div>
+                <div className="four flex flex-col gap-2">
+                    <div className="image">
+                        <Image
+                            src="http://localhost/astha-news/wp-content/uploads/2026/07/prothomalo-bangla_2026-07-11_mpg7sr81_US-Iran-2.jpg"
+                            alt="abc"
+                            width={350}
+                            height={250}
+                            className="object-cover size-full transition-transform duration-300 group-hover:scale-105"
+                        />
+                    </div>
+                    <div className="text">
+                        <h3 className="text-[20px] font-bold text-gray-900">
+                            মার্কিন বোমা হামলা ও প্রাথমিক সমঝোতা ব্যর্থ, ইরান নিয়ে ট্রাম্পের পরের পরিকল্পনা কী
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa dolores modi eveniet totam, quis possimus minima distinctio, tempore numquam, ducimus vitae hic omnis rerum perspiciatis porro dicta maiores illo ab!
+                        </p>
+                        <p className="mt-2 text-[16px] text-gray-500">
+                            ১১ জুলাই, ২০২৬ এ ১১:২৬ AM
+                        </p>
+                    </div>
+                </div>
+                <div className="five flex flex-col gap-2">
+                    <div className="image">
+                        <Image
+                            src="http://localhost/astha-news/wp-content/uploads/2026/07/prothomalo-bangla_2026-07-11_mpg7sr81_US-Iran-2.jpg"
+                            alt="abc"
+                            width={350}
+                            height={250}
+                            className="object-cover size-full transition-transform duration-300 group-hover:scale-105"
+                        />
+                    </div>
+                    <div className="text">
+                        <h3 className="text-[20px] font-bold text-gray-900">
+                            মার্কিন বোমা হামলা ও প্রাথমিক সমঝোতা ব্যর্থ, ইরান নিয়ে ট্রাম্পের পরের পরিকল্পনা কী
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa dolores modi eveniet totam, quis possimus minima distinctio, tempore numquam, ducimus vitae hic omnis rerum perspiciatis porro dicta maiores illo ab!
+                        </p>
+                        <p className="mt-2 text-[16px] text-gray-500">
+                            ১১ জুলাই, ২০২৬ এ ১১:২৬ AM
+                        </p>
+                    </div>
+                </div>
+                <div className="six flex flex-col gap-2">
+                    <div className="image">
+                        <Image
+                            src="http://localhost/astha-news/wp-content/uploads/2026/07/prothomalo-bangla_2026-07-11_mpg7sr81_US-Iran-2.jpg"
+                            alt="abc"
+                            width={350}
+                            height={250}
+                            className="object-cover size-full transition-transform duration-300 group-hover:scale-105"
+                        />
+                    </div>
+                    <div className="text">
+                        <h3 className="text-[20px] font-bold text-gray-900">
+                            মার্কিন বোমা হামলা ও প্রাথমিক সমঝোতা ব্যর্থ, ইরান নিয়ে ট্রাম্পের পরের পরিকল্পনা কী
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa dolores modi eveniet totam, quis possimus minima distinctio, tempore numquam, ducimus vitae hic omnis rerum perspiciatis porro dicta maiores illo ab!
+                        </p>
+                        <p className="mt-2 text-[16px] text-gray-500">
+                            ১১ জুলাই, ২০২৬ এ ১১:২৬ AM
+                        </p>
+                    </div>
+                </div>
+
+                
             </section>
 
 
